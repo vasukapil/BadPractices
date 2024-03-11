6f6d20bb54cba90acad8fa2429afcf70cda32cea
vasukapil2196 <vasukapil2196@gmail.com>
2024-03-12 02:44:59 +0530

    image upload

--- src/components/InputFile.jsx
+++ src/components/InputFile.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const InputFile = () => {
   return (
-    <div>InputFile</div>
+    <div>
+        <input type={"file"} onChange={(e)=>{
+            if((e.target.files[0].type === "image/png" || e.target.files[0].type === "image/pdf") && e.target.files[0].size < 1000000)
+            {
+                console.log(e.target.files[0])
+            }
+            else
+            {
+                alert("Please select a valid file")
+            }
+        }} />
+    </div>
   )
 }
 
